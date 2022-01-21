2a87ef7bfa6135f3ac0992632b2d34eda131babd
Haena <kimhn200605@gmail.com>
2022-01-21 14:37:30 +0900

    클릭 시 마커 위치 변경

--- djangoproject/myapp/static/js/getmap.js
+++ djangoproject/myapp/static/js/getmap.js
@@ -4,38 +4,35 @@ var container = document.getElementById('map');
 			level: 3 //지도 zoom 정도 설정, 숫자가 작을 수록 좁은 영역이 크게 나옴
 		};
 
-		var map = new kakao.maps.Map(container, options);
+		var map = new kakao.maps.Map(container, options);  //지도 생성 및 객체 리턴
 
-        //var markerPosition = new kakao.maps.LatLng(37.49508227061587, 127.12241420996934); //marker 위치 지정
+// 지도를 클릭한 위치에 표출할 마커입니다
+var marker = new kakao.maps.Marker({
+    // 지도 중심좌표에 마커를 생성합니다 
+    position: map.getCenter() 
+}); 
 
-        var marker = new kakao.maps.Marker({ // marker 생성
-            position: map.getCenter() //position: map.getCenter() --> 지도 중심 좌표에 마크 생성
-        });
+// 지도에 마커를 표시합니다
+marker.setMap(map);
 
-        marker.setMap(map); //marker가 지도 위에 그려짐 
-
-
-
-
-        // 지도에 클릭 이벤트를 등록합니다
-        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-        kakao.maps.event.addListener(map, 'click', function(mouseEvent) { 
-            
-            // 클릭한 위도, 경도 정보를 가져옵니다 
-            var latlng = mouseEvent.latLng; 
+// 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
     
-            // 마커 위치를 클릭한 위치로 옮깁니다
-            marker.setPosition(latlng);
+    // 클릭한 위도, 경도 정보를 가져옵니다 
+    var latlng = mouseEvent.latLng; 
     
-            var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-            message += '경도는 ' + latlng.getLng() + ' 입니다';
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+    
+    var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+    
+    var resultDiv = document.getElementById('clickLatlng'); 
+    resultDiv.innerHTML = message;
+    
+});
 
-            var resultDiv = document.getElementById('clickLatlng'); 
-            resultDiv.innerHTML = message;
-        });
-
-
-
-        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤러 생성
-        var zoomControl = new kakao.maps.ZoomControl();
-        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);//kakao.maps.ControlPosition: 위치 지정
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤러 생성
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);//kakao.maps.ControlPosition: 위치 지정 
